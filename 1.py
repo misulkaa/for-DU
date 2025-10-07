45926472ca3e0b21674d1165457d2bab0ab85729
[misulkaa] <misullka@michaelas-MacBook-Air.local>
2025-10-07 20:11:46 +0300

    first commit

---
+++ 1.py
@@ -0,0 +1,4 @@
+#naprogramuj funkciu, ktorá vypíše čísla od 1-10
+
+for i in range(1, 11):
+    print(i)
---
+++ 2a.py
@@ -0,0 +1,6 @@
+#Naprogramuj funkciu, ktorá vypíše čísla od 1-N, N je vstupný parameter Pod seba
+
+N = int(input("parameter n   "))
+
+for i in range(1, N+1):
+    print(i)
